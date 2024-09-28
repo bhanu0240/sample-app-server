09a89fff028825a15928c99861e7f0e9cb7dce37
bhanu0240 <bhanureddy0240@gmail.com>
2024-09-28 17:40:59 +0530

    Worked on adding route to send Hello World text

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3030;
 
+app.get("/",(req,res)=>{
+    return res.send("Hello World");
+});
+
 app.listen(PORT,()=>{
     console.log("Server started running at "+PORT);
 });
